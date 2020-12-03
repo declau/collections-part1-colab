83b6d3fc173d4095cbd7c9434daa4670df44ad75
declau <dec82@hotmail.com>
2020-12-03 11:30:22 -0300

    search by IP

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 //Gerar vai retornar a aplicação de linha de comando pronta
 //para ser executada
@@ -9,5 +15,34 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca de IPs e Nomes de Servidores na Internet"
 
+	//App.commands vai receber um Slice de comandos
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca de IPs",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "github.com",
+				},
+			},
+			Action: buscaIps,
+		},
+	}
+
 	return app
+}
+
+func buscaIps(c *cli.Context) {
+	host := c.String("host")
+
+	//Usando pacote Net
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
