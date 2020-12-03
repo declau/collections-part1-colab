5a730d22d423417ed254c2db9f492c6975084802
declau <dec82@hotmail.com>
2020-12-03 17:19:48 -0300

    server search

--- app/app.go
+++ app/app.go
@@ -15,18 +15,26 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca de IPs e Nomes de Servidores na Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "github.com",
+		},
+	}
+
 	//App.commands vai receber um Slice de comandos
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca de IPs",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "github.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca de IPs",
+			Flags:  flags,
 			Action: buscaIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Busca de Servidores",
+			Flags:  flags,
+			Action: buscaServers,
 		},
 	}
 
@@ -44,5 +52,17 @@ func buscaIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+func buscaServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro := net.LookupNS(host) //NS Name Server
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
